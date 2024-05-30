f6b2ddad69985ef017c92751e880445cff34108c
GoldenHatchet15 <raphsant20@gmail.com>
2024-05-30 16:41:34 -0400

    task3api

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    const selectedAmenities = {};
+
+    $('input[type="checkbox"]').change(function() {
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+
+        if (this.checked) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        const amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('.filters h4').text(`Amenities: ${amenitiesList}`);
+    });
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+});
